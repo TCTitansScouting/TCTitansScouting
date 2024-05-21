e778223e00ee024bbbf9b26b236db0c1e36637f3
preston halverson <160366467+TCTitansScouting@users.noreply.github.com>
2024-05-21 18:41:15 +0000

    another bug fix attempt on download page

--- src/components/FormDownloadPage.vue
+++ src/components/FormDownloadPage.vue
@@ -1,6 +1,8 @@
 <template>
   <FormPage title="Download Data" ref="page">
     <FormGroup :label-type="LabelType.None" :colspan="2" align="center">
+    </FormGroup>
+    <FormGroup :label-type="LabelType.None" :colspan="2" align="center">
       <button @click="clearForm">Save and Clear Form</button>
       <br>
       <button @click="generateQRCode">Generate QR Code</button>
@@ -34,7 +36,6 @@
     </div>
   </dialog>
 </template>
-
 <script setup lang="ts">
 import { defineProps, ref, computed, defineExpose } from "vue";
 import FormPage from "./FormPage.vue";
@@ -44,22 +45,17 @@ import QrcodeVue from "qrcode.vue";
 import { useConfigStore, useWidgetsStore } from "@/common/stores";
 import { useRouter } from "vue-router";
 import QRCode from 'qrcode';
-
 const config = useConfigStore();
 const widgets = useWidgetsStore();
-
 const props = defineProps<{
   data: any;
 }>();
-
 const spreadsheetData = ref('');
 const qrCodeUrl = ref('');
 const excludeHeaders = ref(false);
-
 const router = useRouter();
 const page = ref<InstanceType<typeof FormPage>>();
 const qrContainer = ref<HTMLDialogElement>();
-
 function generateQRCode() {
   const dataText = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
   if (dataText) {
@@ -74,26 +70,21 @@ function generateQRCode() {
     alert('Please enter some form data.')
   }
 }
-
 function clearForm() {
   widgets.save();
   router.go(0);
 }
-
 defineExpose({ title: computed(() => page?.value?.title), setShown: computed(() => page?.value?.setShown) });
 </script>
-
 <style lang="postcss">
 #qr-dialog-contents {
   display: flex;
   flex-direction: column;
   gap: 4px;
   align-items: flex-start;
-
   button {
     align-self: flex-end;
   }
-
   label {
     color: black;
   }
