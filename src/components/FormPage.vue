d0f1240910033a901916865d4c6d27c73445c793
preston halverson <160366467+TCTitansScouting@users.noreply.github.com>
2024-05-21 23:23:45 +0000

    changed from mp4 to gif for animated logo

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -2,10 +2,7 @@
   <div v-show="show">
     <h1 class="page-heading">{{ config.data.heading ?? "Scouting" }}</h1>
     <h3 v-if="teamDesc?.length > 0" class="page-heading">Team: {{ teamDesc }}</h3>
-    <video v-if="config.data.video" autoplay muted loop class="center">
-      <source :src="absoluteVideoPath" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+    <img v-if="config.data.logo" :src="absoluteLogoPath" alt="Cannot load logo file" class="center" />
     <h2 class="page-heading">{{ title }}</h2>
     <div class="grid">
       <slot></slot>
@@ -25,8 +22,8 @@ const widgets = useWidgetsStore();
 
 const teamDesc = $computed(() => widgets.values.find(i => i.name == "Team")?.value.replaceAll(",", ", "));
 
-// Get the full path to the video file
-const absoluteVideoPath = =$computed(() => `${import.meta.env.BASE_URL}assets/${config.data.logo}`);
+// Get the full path to the logo image
+const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/${config.data.logo}`);
 
 let show = $ref(false);
 
@@ -50,10 +47,5 @@ defineExpose({ title: props.title, setShown: (value: boolean) => show = value })
 
 .page-heading {
   text-align: center;
-}
-
-/* Style for the video */
-video {
-  max-width: 100%;
 }
 </style>
