f18eadc051a681a1711414056b0514e368dd22ff
preston halverson <160366467+TCTitansScouting@users.noreply.github.com>
2024-05-28 18:37:32 +0000

    changed form page to add test for display

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -1,16 +1,6 @@
 <template>
-  <div>
-    <b-button v-b-toggle.sidebar-variant>Toggle Sidebar</b-button>
-    <b-sidebar id="sidebar-variant" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-      </div>
-    </b-sidebar>
-  </div>
   <div v-show="show">
+    <RouterLink :to="{ name: 'home' }" style="margin-right: 40px;">Home</RouterLink>
     <h1 class="page-heading">{{ config.data.heading ?? "Scouting" }}</h1>
     <h3 v-if="teamDesc?.length > 0" class="page-heading">Team: {{ teamDesc }}</h3>
     <img v-if="config.data.logo" :src="absoluteLogoPath" alt="Cannot load logo file" class="center" />
