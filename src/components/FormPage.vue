8d1fd5a2721e694a2beba994ec1b26f3fef01a1b
preston halverson <160366467+TCTitansScouting@users.noreply.github.com>
2024-05-28 18:32:16 +0000

    added start of navigation bar

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -1,4 +1,16 @@
 <template>
+  <div>
+    <b-button v-b-toggle.sidebar-variant>Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-variant" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+  </div>
   <div v-show="show">
     <h1 class="page-heading">{{ config.data.heading ?? "Scouting" }}</h1>
     <h3 v-if="teamDesc?.length > 0" class="page-heading">Team: {{ teamDesc }}</h3>
